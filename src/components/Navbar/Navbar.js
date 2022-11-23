715afb10cf75db8ea473d74b827a360445416862
Alons13 <valentino.alonsonovillo@gmail.com>
2022-11-23 20:24:05 -0300

    NavBar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+
+    return  (
+        <><nav className='navbar'>
+            <h1>MarketPlace</h1>
+            <button className='boton'>Inicio</button>
+            <button className='boton'>Catalogo</button>
+            <button className='boton'>Contactanos!</button>
+            <button className='boton'>Inicia</button>
+        </nav><h2>Bienvenido!</h2></>
+        
+    )
+}
+
+export default Navbar
